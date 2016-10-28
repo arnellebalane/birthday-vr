e4742767341990e999705acc48798a3e9a86df85
Arnelle Balane <arnellebalane@gmail.com>
2016-10-28 23:17:40 +0800

    add images for image grid +x

--- images.js
+++ images.js
@@ -48,39 +48,39 @@ var imageGrids = [
         size: 3,
         images: [
             {
-                source: 'images/image-01.jpg',
+                source: 'images/image-10.jpg',
                 offsets: { z: -3, y: -3, x: 0 }
             },
             {
-                source: 'images/image-02.jpg',
+                source: 'images/image-11.jpg',
                 offsets: { z: 0, y: -3, x: 0 }
             },
             {
-                source: 'images/image-03.jpg',
+                source: 'images/image-12.jpg',
                 offsets: { z: 3, y: -3, x: 0 }
             },
             {
-                source: 'images/image-04.jpg',
+                source: 'images/image-13.jpg',
                 offsets: { z: -3, y: 0, x: 0 }
             },
             {
-                source: 'images/image-05.jpg',
+                source: 'images/image-14.jpg',
                 offsets: { z: 0, y: 0, x: 0 }
             },
             {
-                source: 'images/image-06.jpg',
+                source: 'images/image-15.jpg',
                 offsets: { z: 3, y: 0, x: 0 }
             },
             {
-                source: 'images/image-07.jpg',
+                source: 'images/image-16.jpg',
                 offsets: { z: -3, y: 3, x: 0 }
             },
             {
-                source: 'images/image-08.jpg',
+                source: 'images/image-17.jpg',
                 offsets: { z: 0, y: 3, x: 0 }
             },
             {
-                source: 'images/image-09.jpg',
+                source: 'images/image-18.jpg',
                 offsets: { z: 3, y: 3, x: 0 }
             },
         ]
