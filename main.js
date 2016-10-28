4ffd078961692e9c535da9f9d0b6466b0f846f71
Arnelle Balane <arnellebalane@gmail.com>
2016-10-28 20:56:23 +0800

    render given text using letters pixel data

--- main.js
+++ main.js
@@ -1,83 +1,35 @@
-var SEGMENTS_COUNT = 72;
-var VISUALIZATION_RADIUS = 5;
+var TEXT = 'abcdefghijklmnopqrstuvwxyz';
+var PIXEL_SIZE = 0.5;
 
 var scene = document.querySelector('a-scene');
-var segments = [];
-
-generateAudioVisualizationSegments(scene);
+var cursor = 0;
 
 
-var audio = new Audio();
-audio.src = 'audio/happy-birthday.mp3';
-audio.autoplay = true;
-audio.loop = true;
-
-var audioCtx = new AudioContext();
-var source = audioCtx.createMediaElementSource(audio);
-var analyser = audioCtx.createAnalyser();
-
-source.connect(analyser);
-analyser.connect(audioCtx.destination);
-
-var timeDomainData = new Uint8Array(analyser.fftSize);
-var audioDataIndexInterval = Math.floor(analyser.fftSize / SEGMENTS_COUNT);
-
-var frameTimestamp = new Date();
-(function visualize() {
-    requestAnimationFrame(visualize);
-    analyser.getByteTimeDomainData(timeDomainData);
-
-    segments.forEach(function(segment, i) {
-        var data = timeDomainData[i * audioDataIndexInterval] - 128;
-        segment.setAttribute('height', Math.max(0, data / 10));
-    });
-})();
-
-
-
-function generateAudioVisualizationSegments(container) {
-    for (var i = 0; i < SEGMENTS_COUNT; i++) {
-        var segment = generateAudioVisualizationSegment(i);
-        segments.push(segment);
-        scene.appendChild(segment);
+for (var i = 0; i < TEXT.length; i++) {
+    var character = TEXT.charAt(i);
+    if (!letters.hasOwnProperty(character)) {
+        continue;
     }
-}
+    var letter = letters[character];
 
+    for (var row = 0; row < letter.data.length; row++) {
+        for (var column = 0; column < letter.data[row].length; column++) {
+            if (letter.data[row][column] === 0) {
+                continue;
+            }
 
-function generateAudioVisualizationSegment(index) {
-    var angle = calculateAngleAtIndex(index);
-    var position =  calculatePointAtAngle(degreesToRadians(angle), VISUALIZATION_RADIUS);
-    var segment = document.createElement('a-box');
-    segment.setAttribute('color', '#aa0000');
-    segment.setAttribute('width', 0.25);
-    segment.setAttribute('height', 0.25);
-    segment.setAttribute('depth', 0.25);
-    segment.setAttribute('position', [
-        position.x,
-        'ontouchstart' in window ? 0 : 1.6,
-        position.y
-    ].join(' '));
-    segment.setAttribute('rotation', [0, -angle, 0].join(' '));
-    console.log(angle);
-    return segment;
-}
-
-
-function calculatePointAtAngle(angle, distance) {
-    var x = Math.cos(angle) * distance;
-    var y = Math.sin(angle) * distance;
-    return {
-        x: x < 0.00001 && x > -0.00001 ? 0 : x,
-        y: y < 0.00001 && y > -0.00001 ? 0 : y
-    };
-}
-
-
-function calculateAngleAtIndex(index) {
-    return 360 / SEGMENTS_COUNT * index;
-}
-
-
-function degreesToRadians(angle) {
-    return angle * Math.PI / 180;
+            var pixel = document.createElement('a-box');
+            pixel.setAttribute('width', PIXEL_SIZE);
+            pixel.setAttribute('height', PIXEL_SIZE);
+            pixel.setAttribute('depth', PIXEL_SIZE);
+            pixel.setAttribute('color', '#ff0000');
+            pixel.setAttribute('position', [
+                (cursor * PIXEL_SIZE) + (column * PIXEL_SIZE) - 60,
+                (-row * PIXEL_SIZE) - (letter.top * PIXEL_SIZE) + 3,
+                -7
+            ].join(' '));
+            scene.appendChild(pixel);
+        }
+    }
+    cursor += letter.data[0].length + 1;
 }
